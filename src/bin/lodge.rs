926477bb4c600293efaded5535e61e288552e641
Jesse B. Hannah <jesse@jbhannah.net>
2020-02-04 15:13:15 -0700

    Walk source directories, respecting ignore files

--- src/bin/lodge.rs
+++ src/bin/lodge.rs
@@ -1,7 +1,11 @@
 use clap::{App, Arg};
 use dirs::home_dir;
+use ignore::{overrides::OverrideBuilder, WalkBuilder, WalkState};
+use std::path::PathBuf;
 
-fn main() {
+const OVERRIDES: [&str; 2] = ["!.git", "!.hg"];
+
+fn main() -> Result<(), ignore::Error> {
     let home = home_dir().expect("could not determine home directory");
     let home_path = home.to_str().expect("could not get path of home directory");
 
@@ -24,5 +28,55 @@ fn main() {
         )
         .get_matches();
 
-    println!("{:#?}", matches);
+    let sources: Vec<PathBuf> = matches
+        .values_of("SOURCES")
+        .unwrap()
+        .map(PathBuf::from)
+        .collect();
+    let _target = PathBuf::from(matches.value_of("target").unwrap());
+
+    let mut overrides: Vec<OverrideBuilder> = Vec::new();
+
+    for source in sources.iter() {
+        let mut over = OverrideBuilder::new(source);
+
+        for rule in OVERRIDES.iter() {
+            over.add(rule)?;
+        }
+
+        overrides.push(over);
+    }
+
+    if let Some((first, rest)) = sources.split_first() {
+        let mut builder = WalkBuilder::new(first);
+        builder.hidden(false);
+
+        for over in overrides.iter() {
+            builder.overrides(over.build()?);
+        }
+
+        for path in rest {
+            builder.add(path);
+        }
+
+        builder.build_parallel().run(|| {
+            Box::new(|p| {
+                if let Ok(path) = p {
+                    let components = path
+                        .path()
+                        .components()
+                        .rev()
+                        .take(path.depth())
+                        .collect::<Vec<_>>();
+
+                    if !components.is_empty() {
+                        println!("{:?}", components.iter().rev().collect::<Vec<_>>());
+                    }
+                }
+
+                WalkState::Continue
+            })
+        })
+    }
+    Ok(())
 }
